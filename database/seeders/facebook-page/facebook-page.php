2f8a7d27eaade94448a62f2db2f5e9a0aa512fd7
Saheli2704 <myelf.saheli.roy@gmail.com>
2026-05-07 13:56:35 +0530

    enhancement: added facebook page in supported integration with metadata

---
+++ database/seeders/facebook-page/facebook-page.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    [
+        'name' => 'facebook-page',
+        'small_name' => 'FB',
+        'nature' => 'REST API',
+        'category' => 'social-media',
+        'status' => 'active'
+    ]
+];
